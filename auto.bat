08556a24288c1ce1b0f358d486235cf033366cae
olokos <olokos@wp.pl>
2020-04-30 00:15:03 +0200

    Added auto.bat to automate converting translated strings to steam workshop
    Thanks to @TheBadOgre the process of converting files from game to transifex and from transifex to game is now automated!

---
+++ auto.bat
@@ -0,0 +1,3 @@
+java -jar LocaleParser-0.1.0-SNAPSHOT.jar folder_supply translations/pl translations/en translations/supplied_pl yaml
+java -jar LocaleParser-0.1.0-SNAPSHOT.jar folder_to_eu4 translations/supplied_pl translations/supplied_pl_eu4 polish
+pause
