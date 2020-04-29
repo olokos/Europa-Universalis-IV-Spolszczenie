3ace48583abd2da6fdcb07d042d58f23c2914dac
TheBadOgre <admin@rafkos.net>
2020-04-30 00:45:16 +0200

    Version updated

--- auto.bat
+++ auto.bat
@@ -1,4 +1,4 @@
 echo "Converting translated files from Transifex into the EU4 format for workshop delivery."
-java -jar LocaleParser-0.1.0-SNAPSHOT.jar folder_supply translations/pl translations/en translations/supplied_pl yaml
-java -jar LocaleParser-0.1.0-SNAPSHOT.jar folder_to_eu4 translations/supplied_pl translations/supplied_pl_eu4 polish
+java -jar LocaleParser-0.1.1-SNAPSHOT.jar folder_supply translations/pl translations/en translations/supplied_pl yaml
+java -jar LocaleParser-0.1.1-SNAPSHOT.jar folder_to_eu4 translations/supplied_pl translations/supplied_pl_eu4 polish
 pause
