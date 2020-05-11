c8b1a835d1a5ce8581caf18fe84eecdce5e9f1fa
olokos <olokos@wp.pl>
2020-05-11 20:34:00 +0200

    auto.bat - updated the echos, to calm down the user
    We're removing temp directory, in case it existed before
    But in case it was removed earlier, which it should be then it will throw an error - no such file or directory
    
    That is normal and we should make the user know about this.

--- auto.bat
+++ auto.bat
@@ -5,7 +5,8 @@ set project_folder="Spolszczenie_EUIV_Community_Edition"
 set parser_version="0.1.8"
 
 :: LOGIC
-echo "Converting translated files from Transifex into the EU4 format for workshop delivery and building mod."
+echo "Converting translated files from Transifex into the EU4 format for workshop delivery and automated mod building."
+echo "Removing temp directory - might say does not exist. That is normal, just a precaution."
 rd /s /q temp
 java -jar "tools\\LocaleParser\\bin\\LocaleParser-%parser_version%-SNAPSHOT.jar" "folder_supply" "translations\\%language%" "translations\\en" "temp\\supply" yaml
 java -jar "tools\\LocaleParser\\bin\\LocaleParser-%parser_version%-SNAPSHOT.jar" "folder_to_eu4" "temp\\supply" "temp\\eu4" "%charset%"
