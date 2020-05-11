b40246834bd94e444a7a4a6137bae544ffd4b33a
olokos <olokos@wp.pl>
2020-05-11 20:38:34 +0200

    auto.bat - make the echo output more verbose

--- auto.bat
+++ auto.bat
@@ -5,10 +5,12 @@ set project_folder="Spolszczenie_EUIV_Community_Edition"
 set parser_version="0.1.8"
 
 :: LOGIC
-echo "Converting translated files from Transifex into the EU4 format for workshop delivery and automated mod building."
+echo "Starting conversion of translated files from Transifex into the EU4 format for workshop delivery and automated mod building."
 echo "Removing temp directory - might say does not exist. That is normal, just a precaution."
 rd /s /q temp
+echo "Starting compiling latest source strings directory for EU4 format into the temp supply directory"
 java -jar "tools\\LocaleParser\\bin\\LocaleParser-%parser_version%-SNAPSHOT.jar" "folder_supply" "translations\\%language%" "translations\\en" "temp\\supply" yaml
+echo "Starting compiling latest translations to desired language - in this case - polish."
 java -jar "tools\\LocaleParser\\bin\\LocaleParser-%parser_version%-SNAPSHOT.jar" "folder_to_eu4" "temp\\supply" "temp\\eu4" "%charset%"
 cd "%project_folder%"
 echo "Removing old localisations"
@@ -17,5 +19,7 @@ mkdir localisation
 cd ../
 echo "Copying new localisation"
 xcopy /s temp\\eu4 "%project_folder%\\localisation"
+echo "Cleaning up temp directory."
 rd /s /q temp
+echo "Temp directory cleaned up, goodbye, have a nice day! :smile:
 pause
