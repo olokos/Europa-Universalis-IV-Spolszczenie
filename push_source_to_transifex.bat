a53cf6e7f3545196a96654d72f74e4d8c89403bf
DrSzuriad <darek.kluczewski@gmail.com>
2022-04-05 22:37:20 +0200

    Update transifex CLI to 3.0

--- push_source_to_transifex.bat
+++ push_source_to_transifex.bat
@@ -1,7 +1,7 @@
 echo "Pushing localisation files to Transifex"
 :: Force push all translations
 ::Push source
-tx.exe push -s --parallel
+tx.exe push -s
 ::Push translations
 ::tx.exe push -t -f --parallel
 pause
