4f0354f053729a3db7802eb5827982ef3fa8804b
DrSzuriad <43809250+DrSzuriad@users.noreply.github.com>
2023-04-19 01:52:27 +0200

    Update pl file to 1.35.0

--- push_source_to_transifex.bat
+++ push_source_to_transifex.bat
@@ -1,7 +1,7 @@
 echo "Pushing localisation files to Transifex"
 :: Force push all translations
 ::Push source
-tx.exe push -s
+tx.exe push -s -f
 ::Push translations
 ::tx.exe push -t -f --parallel
 pause
